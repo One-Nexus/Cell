155b63dcd57a862e355f3be120b046e752892465
Edmund Reed <esr360@live.com>
2021-12-31 04:40:47 +0000

    cleanup + adds uid scoping

--- build/sass-concat.js
+++ build/sass-concat.js
@@ -5,40 +5,30 @@ const fs = require('fs');
 const files = [
   // vendor
   './node_modules/Sass-Boost/dist/_sass-boost.scss',
-  // config
+  // core stuff
   './src/_config.scss',
+  './src/_parse-cq.scss',
   // atoms
   './src/atoms/_display.scss',
   './src/atoms/_position.scss',
   './src/atoms/_visibility.scss',
   // utilities
-  './src/utilities/_create-config.scss',
   './src/utilities/_create-selector-from-context.scss',
   './src/utilities/_css-properties.scss',
-  './src/utilities/_enabled.scss',
-  './src/utilities/_get-module-name.scss',
-  './src/utilities/_get-param.scss',
-  './src/utilities/_merge-css-maps.scss',
-  './src/utilities/_module-tree.scss',
-  './src/utilities/_option.scss',
+  './src/utilities/_get-module.scss',
+  './src/utilities/_get-namespace.scss',
   './src/utilities/_remove-junk.scss',
   './src/utilities/_remove-modifiers.scss',
-  './src/utilities/_parse-cq.scss',
-  './src/utilities/_selector-to-map.scss',
-  './src/utilities/_setting.scss',
-  './src/utilities/_strip-glue.scss',
+  './src/utilities/_shift-pseudo-elements.scss',
   './src/utilities/_theme.scss',
   './src/utilities/_this.scss',
-  './src/utilities/_value-enabled.scss',
   // mixins
   './src/mixins/_module.scss',
   './src/mixins/_component.scss',
   './src/mixins/_modifier.scss',
   './src/mixins/_extend.scss',
   './src/mixins/_context.scss',
-  './src/mixins/_option.scss',
   './src/mixins/_pseudo-state.scss',
-  './src/mixins/_value.scss',
   './src/mixins/_wrapper.scss'
 ];
 
